3b5586980233854f4e3d7135d5b74316175e3404
Orhan Cakan <orhan.cakan@fabasoft.com>
2016-08-09 14:38:59 +0200

    modifications needed for building with CentOS 7.2

--- xtra/rhel7/build_tpm-tools.sh
+++ xtra/rhel7/build_tpm-tools.sh
@@ -15,13 +15,7 @@ pkg=$dir-7
 spec=dist/tpm-tools.spec
 specf=${spec##*/}
 
-ext1=ftp://rpmfind.net/linux/centos/7.1.1503/os/x86_64/Packages/opencryptoki-devel-3.2-4.1.el7.x86_64.rpm
-ext1f=${ext1##*/}
-dep1=trousers-0.3.13-1.x86_64.rpm
-dep2=trousers-devel-0.3.13-1.x86_64.rpm
-   
 if [ "$action" == "1" -o -z "$action" ]; then
-   [ -f src/$ext1f ] || wget $ext1 -P src
    [ -f src/$file ] || wget $url -P src
    [ -d work/$dir ] && rm -rf work/$dir/
    (
@@ -29,7 +23,7 @@ if [ "$action" == "1" -o -z "$action" ]; then
    tar zxf ../src/$file
    cd $dir
    id tss &> /dev/null || sudo useradd -r tss
-   sudo yum install -y automake autoconf libtool openssl openssl-devel ../../src/$ext1f gtk+ ~/rpmbuild/RPMS/$dep1 ~/rpmbuild/RPMS/$dep2
+   sudo yum install -y opencryptoki-devel automake autoconf libtool openssl openssl-devel gtk+ trousers trousers-devel
    sudo ln -s /usr/lib64/libtspi.so.1 /usr/lib64/libtspi.so
    ./configure
    )
@@ -46,8 +40,6 @@ if [ "$action" == "2" -o "$action" == "3" -o -z "$action" ]; then
       mock -r rhel --clean
    fi
    home=$( echo ~makerpm )
-   mock -r rhel --yum-cmd localinstall ~/rpmbuild/RPMS/$dep1
-   mock -r rhel --yum-cmd localinstall ~/rpmbuild/RPMS/$dep2
-   mock -r rhel --yum-cmd localinstall src/${ext1##*/}
+   mock -r rhel --yum-cmd install opencryptoki-devel trousers trousers-devel
    mock -r rhel --resultdir=$home/rpmbuild/RPMS/ ~/rpmbuild/SRPMS/$pkg.src.rpm --no-clean --no-cleanup-after
 fi
